b3652590e83610317a6c8d0b595de3ac55a74e12
midnight33 <gra33@bk.ru>
2023-03-11 08:29:10 +0300

    Задача 21

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+int[] tochka = new int[6];
+System.Console.WriteLine("введите x1: ");
+tochka[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите x2: ");
+tochka[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите y1: ");
+tochka[2] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите y2: ");
+tochka[3] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите z1: ");
+tochka[4] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите z2: ");
+tochka[5] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Math.Sqrt(Math.Pow(tochka[1]-tochka[0],2)+Math.Pow(tochka[3]-tochka[2],2)+Math.Pow(tochka[5]-tochka[4],2)));
